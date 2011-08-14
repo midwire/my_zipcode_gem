6053704c77bf958a9bf8ea27e370baa57410e2a5
Midwire <chris@midwiretech.com>
2011-08-14 18:00:41 -0500

    Fixed cucumber step to overwrite existing files. Updated .gitignore and travis.yml

--- features/step_definitions/rails_setup_steps.rb
+++ features/step_definitions/rails_setup_steps.rb
@@ -9,8 +9,8 @@ end
 Given %{a new migrated Rails app} do
   # Don't delete the rails app
   FileUtils.mkdir_p("tmp")
-  system("rails new tmp/rails_app").should be_true
-  system("ln -s ../../../lib/generators tmp/rails_app/lib/generators").should be_true
+  system("rails new tmp/rails_app -f").should be_true
+  system("ln -sf ../../../lib/generators tmp/rails_app/lib/generators").should be_true
   @current_directory = File.expand_path("tmp/rails_app")
   When %{I run "rails g my_zipcode_gem:models"}
   Then %{I should successfully run "rake db:migrate"}
