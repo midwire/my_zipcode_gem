d8de3b4fcf1f781d157c0580da4936a116584c45
ChakreshwarSharma-BTC <chakreshwar.sharma@botreetechnologies.com>
2016-05-22 14:55:00 +0530

    Update as per Rails 4

---
+++ lib/generators/us_zipcode/templates/county_model.rb
@@ -0,0 +1,17 @@
+require 'memoist'
+class County < ActiveRecord::Base
+  extend Memoist
+
+  belongs_to :state
+  has_many :zipcodes
+  
+  validates :name, :uniqueness => {:scope => :state_id, :case_sensitive => false}, :presence => true
+  
+  scope :without_zipcodes, -> { joins("LEFT JOIN zipcodes ON zipcodes.county_id = counties.id").where("zipcodes.county_id IS NULL")}
+  scope :without_state, -> {where("state_id IS NULL")}
+  
+  def cities
+    zipcodes.map(&:city).sort.uniq
+  end
+  memoize :cities
+end
---
+++ lib/generators/us_zipcode/templates/state_model.rb
@@ -0,0 +1,15 @@
+require 'memoist'
+class State < ActiveRecord::Base
+  extend Memoist
+
+  has_many :zipcodes
+  has_many :counties
+
+  validates :abbr, :uniqueness => { :case_sensitive => false }, :presence => true
+  validates :name, :uniqueness => { :case_sensitive => false }, :presence => true
+  
+  def cities
+    zipcodes.map(&:city).sort.uniq
+  end
+  memoize :cities
+end
