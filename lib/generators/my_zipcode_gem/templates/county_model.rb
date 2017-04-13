2c8b9bfc6e5ec35cd5ec2f3a84f5de6decb63bd0
Chris Blackburn <chris@midwiretech.com>
2017-04-13 12:54:09 -0500

    Fix style issues
    …  and comment out most of the .rubocop.yml settings to allow for defaults until we decide what we want

--- lib/generators/my_zipcode_gem/templates/county_model.rb
+++ lib/generators/my_zipcode_gem/templates/county_model.rb
@@ -8,14 +8,15 @@ class County < ActiveRecord::Base
   validates :name, uniqueness: { scope: :state_id, case_sensitive: false },
                    presence: true
 
-  scope :without_zipcodes, -> {
+  scope :without_zipcodes, lambda {
     county = County.arel_table
     zipcodes = Zipcode.arel_table
-    zipjoin = county.join(zipcodes, Arel::Nodes::OuterJoin)
+    zipjoin = county
+        .join(zipcodes, Arel::Nodes::OuterJoin)
         .on(zipcodes[:county_id].eq(county[:id]))
     joins(zipjoin.join_sources).where(zipcodes[:county_id].eq(nil))
   }
-  scope :without_state, -> {
+  scope :without_state, lambda {
     where(state_id: nil)
   }
 
